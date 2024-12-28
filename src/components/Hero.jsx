3e0bc4dee67c19c1306afc830d5252adc81b8dc8
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-12-27 23:47:32 -0400

    responsive hero text position updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,18 +5,25 @@ import { FaArrowDownLong } from "react-icons/fa";
 const Hero = () => {
   return (
     <div className="relative">
-      {/* Image  */}
+      {/* Image */}
       <div className="flex justify-center items-center">
         <img src={hero} className="w-full h-auto" />
       </div>
 
       {/* Text */}
       <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-my-gray">
-        <p className="text-4xl font-semibold absolute top-[39%] left-[10%]">
+        {/* "web design" */}
+        <p className="text-xl lg:text-2xl xl:text-3xl font-semibold absolute top-[35%] left-[5%] sm:top-[37%] sm:left-[8%] md:top-[39%] md:left-[10%]">
           web design
         </p>
-        <h1 className="text-8xl font-bold">Kavita Thomas</h1>
-        <p className="text-4xl font-semibold absolute bottom-[39%] right-[10%]">
+
+        {/* "Kavita Thomas" */}
+        <h1 className="text-5xl md:text-5xl lg:text-6xl xl:text-8xl p-4 font-bold text-center">
+          Kavita Thomas
+        </h1>
+
+        {/* "software development" */}
+        <p className="text-xl lg:text-2xl xl:text-3xl font-semibold absolute bottom-[35%] right-[5%] sm:bottom-[37%] sm:right-[8%] md:bottom-[39%] md:right-[10%]">
           software development
         </p>
       </div>
