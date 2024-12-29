baf70535673b602234f36e2f5129b1f3328ec4db
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-12-28 23:37:46 -0400

    fix: :bug: circle headshot bug fixed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,37 +3,62 @@ import headshot from "../assets/headshot.png";
 
 const AboutMe = () => {
   return (
-    <div className="flex flex-col md:mt-32 md:flex-row justify-center items-center md:items-start md:space-x-10">
-      {/* Headshot */}
-      <div className="md:mr-8 rounded-full w-96 h-96 overflow-hidden">
-        <img
-          src={headshot}
-          alt="Kavita Thomas Headshot"
-          className="w-full h-full object-cover"
-        />
+    <div className="flex flex-col items-center md:mt-16">
+      {/* Wave Text */}
+      <div className="font-title sm:text-2xl md:text-3xl">
+        <svg
+          className="w-[1000px] h-[200px]"
+          viewBox="0 0 400 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <path
+              id="wave"
+              d="M10,50 C60,10 140,90 200,50 C260,10 340,90 390,50"
+              fill="transparent"
+            />
+          </defs>
+          <text fill="#F2F2F2" fontWeight="bold">
+            <textPath href="#wave" textAnchor="middle" startOffset="50%">
+              Hi! I'm Kavita
+            </textPath>
+          </text>
+        </svg>
       </div>
 
-      {/* About Me text */}
-      <div className="text-my-gray max-w-lg text-left md:text-xl">
-        <p className="my-4">
-          I am a fourth year computing student graduating in Spring 2025 at
-          Queen’s University in Kingston, Ontario. I have a specific interest in
-          front-end and full-stack development, along with web and UI/UX design.{" "}
-        </p>
-        <p>
-          I am passionate about combining my love for art and technology to
-          build user-focused designs and tools that will help people and
-          businesses thrive. My goal is to break through barriers in technology
-          and make tech accessible to women and marginalized peoples, which I
-          practice as a member of Queen’s Women in Computing, a student-run
-          organization dedicated to exploring technology and embracing
-          empowerment.
-        </p>
-        <p className="my-4">
-          Outside of my work in tech, you can often find me channeling my love
-          for design through other mediums such as crochet, or going outside for
-          a run or hike!
-        </p>
+      <div className="flex flex-col items-center xl:flex-row justify-evenly lg:items-center w-full">
+        {/* Headshot */}
+        <div className="w-72 h-72 rounded-full overflow-hidden md:w-80 md:h-80 mb-8 xl:mb-0 mr-4 ">
+          <img
+            src={headshot}
+            alt="Kavita Thomas Headshot"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* About Me text */}
+        <div className="text-my-gray max-w-2xl lg:flex-grow text-left md:text-xl">
+          <p className="my-4">
+            I am a fourth year Computing student graduating in Spring 2025 from
+            Queen’s University in Kingston, Ontario. I have a specific interest
+            in front-end and full-stack development, along with web and UI/UX
+            design.
+          </p>
+          <p>
+            I am passionate about combining my love for art and technology to
+            build user-focused designs and tools that will help people and
+            businesses thrive. My goal is to break through barriers in
+            technology and make tech accessible to women and marginalized
+            peoples, which I practice as a member of Queen’s Women in Computing,
+            a student-run organization dedicated to exploring technology and
+            embracing empowerment.
+          </p>
+          <p className="my-4">
+            Outside of my work in tech, you can often find me channeling my love
+            for design through other mediums such as crochet and paint, or
+            participating in dance classes or enjoying the outdoors!
+          </p>
+        </div>
       </div>
     </div>
   );
