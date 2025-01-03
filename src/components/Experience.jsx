d8c1cf29e38415cb55a7b213ceaf5fa319170cf6
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2025-01-02 22:49:04 -0400

    Added experience info

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,29 +13,33 @@ const Experience = () => {
       title: "Instructional Designer",
       place: "Queen's Law",
       year: "2025",
-      content:
-        "Tailwind CSS is a utility-first CSS framework for building custom designs quickly and efficiently.",
+      content1:
+        "Developed interactive learning web component templates using HTML5 and Bootstrap CSS for use in the Queen's Faculty of Law online courses. The reusable templates allow course developers to build courses quickly and without code, making work more efficient. The templates also rebrand the previous UI of the courses, and provide different ways to display content for a modern, clean and organized learning experience.",
     },
     {
       title: "Web Developer",
       place: "Queen's Women in Computing (QWIC)",
       year: "2025",
-      content:
-        "Tailwind CSS allows for rapid development, responsive designs, and complete control over styling.",
+      content1:
+        "Designed and developing the QWIC website to market resources, merchandise and events. Working in a team of 3, I prototyped 5 different pages in Figma, then developed the front-end using Next.js, Tailwind CSS and Vercel. The next steps of this project will be buiding the backend to include an admin panel.",
+      content2:
+        "As co-lead web developer, I mentor the first-year web developer by providing 1:1 guidance, tutorials, and resources for front-end development in this project.",
     },
     {
       title: "UI/UX Designer and Front-end Developer",
       place: "Queen's Web Development Club (QWeb)",
       year: "2024",
-      content:
-        "Tailwind CSS can be installed via npm or using a CDN for quick prototyping.",
+      content1:
+        "In a 4-month development sprint, I led a team of 3 in the design of a student association web app, implementing and developing brand guidelines, and wireframing and prototyping for web development in Figma. I was also responsible for developing the front-end of multiple pages, where I successfully learned and implemented React.js, Tailwind, Git, and debugging procedures over the 4 months.",
     },
     {
       title: "Marketing, Communications, and Publications Assistant",
       place: "Career Services",
       year: "2023",
-      content:
-        "Tailwind CSS can be installed via npm or using a CDN for quick prototyping.",
+      content1:
+        "Led the major revision of 3 Career Services Magazines, and redesigned and published 3 rebranded magazines ready for print and distribution in the new academic year. Leading this project, I learned valuable project management techniques and used Microsoft Planner to coordinate contributors and design tasks, ensuring deadlines and specifications were met.",
+      content2:
+        "Furthermore, I designed other print and digital promotional media such as posters, newsletters, stickers, and social media posts, successfully marketing Career Services events and resources to employers and students.",
     },
   ];
 
@@ -93,10 +97,11 @@ const Experience = () => {
             </button>
             <div
               className={`transition-[max-height] duration-300 ease-in-out overflow-hidden ${
-                activeIndex === index ? "max-h-40 p-6" : "max-h-0"
-              }  text-my-gray`}
+                activeIndex === index ? "h-auto p-6" : "h-0"
+              } text-my-gray`}
             >
-              {item.content}
+              {item.content1 && <p className="mb-4">{item.content1}</p>}
+              {item.content2 && <p className="mb-4">{item.content2}</p>}
             </div>
           </div>
         ))}
