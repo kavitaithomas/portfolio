38ce9b0d0d9e3b286e74a5ddf3e48fe05b081cad
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2025-02-24 11:43:43 -0500

    responsive fixes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,40 +18,40 @@ const AboutMe = () => {
             delaySpeed={1000}
           />
         </h1>
-        <div className="flex flex-col items-center xl:flex-row justify-evenly w-full">
-          {/* Headshot with Link*/}
-          <a
-            href="https://www.linkedin.com/in/kavitaithomas"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="relative w-72 h-80 rounded-lg border-8 border-my-gray overflow-hidden mb-8 xl:mb-0 mr-4 group"
-          >
-            {/* Profile Image */}
-            <img
-              src={headshot}
-              alt="Kavita Thomas Headshot"
-              className="w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-50"
-            />
 
-            {/* LinkedIn Icon - Appears on Hover */}
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <AiFillLinkedin
-                size={90}
-                color="#241527"
-                className="p-2 rounded-full shadow-lg"
+        <div className="flex flex-col items-center xl:flex-row xl:space-x-24 justify-center w-full px-6">
+          <div className="flex justify-center">
+            <a
+              href="https://www.linkedin.com/in/kavitaithomas"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative w-80 h-80 rounded-xl border-4 border-my-gray overflow-hidden mb-8 xl:mb-0 group"
+            >
+              <img
+                src={headshot}
+                alt="Kavita Thomas Headshot"
+                className="w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-50"
               />
-            </div>
-          </a>
 
-          {/* About Me text */}
-          <div className="text-my-gray max-w-2xl lg:flex-grow text-left text-xl">
-            <p className="my-4">
-              I am a fourth year Computing student graduating in Spring 2025
+              {/* LinkedIn Icon - Appears on Hover */}
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <AiFillLinkedin
+                  size={90}
+                  color="#241527"
+                  className="p-2 rounded-full shadow-lg"
+                />
+              </div>
+            </a>
+          </div>
+
+          <div className="text-my-gray text-lg sm:text-xl max-w-3xl leading-relaxed">
+            <p className="mb-4">
+              I am a fourth-year Computing student graduating in Spring 2025
               from Queen’s University in Kingston, Ontario. I have a specific
               interest in front-end and full-stack development, and enjoy web
               and UI/UX design.
             </p>
-            <p>
+            <p className="mb-4">
               I am passionate about combining my love for design and technology
               to build user-focused tools that will help people and businesses
               thrive. My goal is to break through barriers in technology and
@@ -60,7 +60,7 @@ const AboutMe = () => {
               organization dedicated to exploring technology and embracing
               empowerment.
             </p>
-            <p className="my-4">
+            <p className="mb-4">
               Outside of my work in tech, you can often find me channeling my
               love for design through other mediums such as crochet and paint,
               or participating in dance classes or enjoying the outdoors!
